753a85adcbc7cdab49dc2698266c1fa64f456802
billythedummy <dhy1996@live.com.sg>
2023-08-12 14:05:49 +0800

    who linted node_modules

--- app/js/index.js
+++ app/js/index.js
@@ -3,7 +3,7 @@
 // we have to use relative imports straight from node_modules like this
 // because axum is serving the static files in dev mode, not vite,
 // and it knows nothing about resolving esm imports
-import * as _setup from "htmx.org/dist/htmx.js";
+import * as _setup from "../node_modules/htmx.org/dist/htmx.js";
 // the import is a IIFE that sets global var `htmx`
 // @ts-ignore
 window.htmx = htmx;
